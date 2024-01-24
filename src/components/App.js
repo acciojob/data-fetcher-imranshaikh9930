0ab4ef8f6c503e0d56831ad13622254e412ca379
Imran shaikh <imranpathan7040@gmail.com>
2024-01-24 19:19:18 +0530

    code update2

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,17 @@
-
-import React,{useState,useEffect} from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   // Fetch data when the component mounts for the first time
   useEffect(() => {
     const fetchData = () => {
-      fetch('https://dummyjson.com/products')
+      fetch("https://dummyjson.com/products")
         .then((response) => {
           if (!response.ok) {
-            throw new Error('Failed to fetch data');
+            throw new Error("");
           }
           return response.json();
         })
@@ -37,15 +35,17 @@ const App = () => {
     <div id="root">
       <h1>Data Fetched from API</h1>
       {loading && <p>Loading...</p>}
-      {error && <p>No data found</p>}
-      {product && (
+      {error ? (
+        <p>{error}</p>
+      ) : product ? (
         <div>
           <pre>{JSON.stringify(product, null, 2)}</pre>
         </div>
+      ) : (
+        <p>No Data found</p>
       )}
-
     </div>
   );
-}
+};
 
-export default App
+export default App;
